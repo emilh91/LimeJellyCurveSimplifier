2e46364572b132b47a660b6a84f4e188abc557b2
Simon Gellis <simongellis@gmail.com>
2015-04-14 20:39:34 -0400

    The LimeJelly curve simplifier can now theoretically simplify certain curves

--- LimeJelly.Core/Impl/RamerDouglasPeuckerSimplifier.cs
+++ LimeJelly.Core/Impl/RamerDouglasPeuckerSimplifier.cs
@@ -12,7 +12,42 @@ namespace LimeJelly.Core.Impl
 
         protected override ICurve SimplifyImpl(ICurve curve, double epsilon)
         {
-            return curve; //wgas
+            return new CurveImpl(SimplifyImpl(curve.Points, epsilon));
+        }
+
+        private IEnumerable<IPoint> SimplifyImpl(IEnumerable<IPoint> region, double epsilon)
+        {
+            var points = region.ToList();
+            if (points.Count < 2)
+                return points; // A point or an empty list are about as simple as can be
+
+            var farthest = 0;
+            var farthestDist = epsilon;
+            var start = points.First(); var end = points.Last();
+
+            // find the point farthest from the line containing both endpoints of the curve.
+            for(var i = 1; i < points.Count - 1; ++i)
+            {
+                var dist = points[i].DistanceToLine(start, end);
+                if (dist > farthestDist)
+                {
+                    farthest = i;
+                    farthestDist = dist;
+                }
+            }
+            if (farthestDist <= epsilon)
+            {
+                // no point in this region is farther than epsilon from the curve,
+                // so we discard everything but the endpoints.
+                return new[] { start, end };
+            }
+
+            // Recurse on the sections before and after the farthest point,
+            // and be sure to keep the farthest point too (which is definitely > epsilon)
+            var firstHalf = SimplifyImpl(points.Take(farthest + 1), epsilon);
+            var secondHalf = SimplifyImpl(points.Skip(farthest), epsilon);
+
+            return firstHalf.Concat(secondHalf.Skip(1)); // skip farthest so we don't return it twice
         }
     }
 }
