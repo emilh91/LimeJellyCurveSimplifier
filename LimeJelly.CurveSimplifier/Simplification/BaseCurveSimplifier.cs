2068d340ab2a7d88008df3132570065bbbf37c2f
Simon Gellis <simongellis@gmail.com>
2015-04-28 01:26:34 -0400

    BaseCurveSimplifier can handle iteratively removing points instead of adding

--- LimeJelly.CurveSimplifier/Simplification/BaseCurveSimplifier.cs
+++ LimeJelly.CurveSimplifier/Simplification/BaseCurveSimplifier.cs
@@ -11,7 +11,7 @@ namespace LimeJelly.CurveSimplifier.Simplification
     abstract class BaseCurveSimplifier : ICurveSimplifier
     {
         private List<Vector2> _points;
-        private List<Tuple<int, int>> _solutionSegments;
+        private LinkedList<Tuple<int, int>> _solutionSegments;
         private HashSet<int> _solutionPoints;
 
         /// <summary>
@@ -33,7 +33,7 @@ namespace LimeJelly.CurveSimplifier.Simplification
         protected BaseCurveSimplifier(IEnumerable<Vector2> points)
         {
             _points = points.ToList();
-            _solutionSegments = new List<Tuple<int, int>>();
+            _solutionSegments = new LinkedList<Tuple<int, int>>();
             _solutionPoints = new HashSet<int>();
         }
 
@@ -44,7 +44,7 @@ namespace LimeJelly.CurveSimplifier.Simplification
         /// <param name="end">The index of the end vertex in Points.</param>
         protected void AddSegmentToSolution(int start, int end)
         {
-            _solutionSegments.Add(Tuple.Create(start, end));
+            _solutionSegments.AddLast(Tuple.Create(start, end));
             _solutionPoints.Add(start);
             _solutionPoints.Add(end);
         }
@@ -56,6 +56,48 @@ namespace LimeJelly.CurveSimplifier.Simplification
         protected void AddPointToSolution(int index)
         {
             _solutionPoints.Add(index);
+        }
+
+        /// <summary>
+        /// Marks the entire curve as part of the "simplified" version.
+        /// Useful when iteratively removing points from the solution.
+        /// </summary>
+        protected void AddEntireCurveToSolution()
+        {
+            _solutionSegments = new LinkedList<Tuple<int, int>>();
+            for(var i = 0; i < _points.Count - 1; ++i)
+            {
+                _solutionSegments.AddLast(Tuple.Create(i, i + 1));
+            }
+            _solutionPoints = new HashSet<int>(Enumerable.Range(0, _points.Count));
+
+        }
+
+        /// <summary>
+        /// Removes a vertex from the simplified curve.
+        /// Also joins any segments intersecting that vertex.
+        /// </summary>
+        /// <param name="index">The index of the vertex to remove.</param>
+        protected void RemovePointFromSolution(int index)
+        {
+            _solutionPoints.Remove(index);
+            var node = _solutionSegments.First;
+            while (node != null && node.Value.Item1 <= index)
+            {
+                if (node.Value.Item1 == index)
+                {
+                    if (node.Previous != null)
+                        node.Previous.Value = Tuple.Create(node.Previous.Value.Item1, node.Value.Item2);
+                    _solutionSegments.Remove(node);
+                }
+                if (node.Value.Item2 == index)
+                {
+                    if (node.Next != null)
+                        node.Next.Value = Tuple.Create(node.Value.Item1, node.Next.Value.Item2);
+                    _solutionSegments.Remove(node);
+                }
+                node = node.Next;
+            }
         }
 
         public abstract IVisualizationStep NextStep();
